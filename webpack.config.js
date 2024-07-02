8b94d4069bdd2ca3fb67ad01f08464a9ec86fc4f
Wolfdog940 <oscarurrutia940@gmail.com>
2024-07-02 18:42:41 +0200

    ok

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,11 @@
 const path = require('path');
 
 module.exports = {
-  entry: './dist/server.js',
-  output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist'),
+    entry: './src/index.js',
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist'),
   },
   mode: 'development',
+  target: "node",
 };
